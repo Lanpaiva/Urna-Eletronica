a91b33fa0ae629f32fcc313646ce5f947ddcf88a
Alan Paiva <allanpaiva21@outlook.com>
2022-02-16 15:33:57 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,57 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        nome: 'Joao de Tal',
+        partido: 'AWS',
+        fotos:[
+          {url:'img4.jpeg', legenda: 'Vereador'}
+        ]
+      },
+      {
+         numero: '77222',
+         nome: 'Beltrano da Silva',
+         partido: 'HTTP',
+         fotos:[
+          {url:'img3.jpeg', legenda: 'vereador'}
+         ]
+        },
+      ]
+    },
+    {
+      titulo: 'PREFEITO',
+      numeros:2,
+      candidatos: [
+         {
+           numero:'99',
+            nome: 'Ciclano',
+            partido: 'PHP',
+            vice: 'Cic',
+            fotos:[
+             {url:'img1.jpeg.png', legenda: 'Prefeita'},
+             {url:'img7.jpeg', legenda: 'Vice-Prefeita', small: true}
+            ]
+
+          },
+          {
+              numero: '84',
+              nome: 'Zulano',
+              partido: 'PHP',
+              vice: 'Zul',
+              fotos:[
+                {url:'img5.jpeg', legenda: 'Prefeito'},
+                {url:'img6.jpeg', legenda: 'Vice-Prefeita', small: true}
+
+              ]
+          },
+      ]
+   }
+];
+
+
+
+
+
